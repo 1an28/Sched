dadd784dc6bf194b6456cd65ebe1b054dbd00596
1an28 <14.nya.28@gmail.com>
2020-10-07 11:37:27 +0900

    create a structure.

--- source/Popup/Clock.tsx
+++ source/Popup/Clock.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 
+type Props = {
+    now: String;
+};
+
+const ClockScale: React.FC = () => { // text pannel
+    return (
+        <h1>clock scale</h1>
+    );
+};
+
+const ClockHands: React.FC<Props> = ( props ) => { // hands
+    return (
+        <h1> {props.now} </h1>
+    );
+};
+
+const ClockApplication: React.FC<Props> = ( props ) => { // clock app
+    return (
+        <section>
+            <div> <ClockScale /></div>
+            <div> <ClockHands now = {props.now} /> </div>
+        </section>
+    );
+};
+
+
 const Clock: React.FC = () => {
     return (
-        <h1>Clock Application</h1>
+        <ClockApplication now = "123" />
     );
 };
 
