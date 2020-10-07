f17aea83cd0915ba6a4cfc5beb81f5891544ff1b
1an28 <14.nya.28@gmail.com>
2020-10-07 12:33:42 +0900

    add a clock handle.

--- source/Popup/Clock.tsx
+++ source/Popup/Clock.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {
-    now: String;
+    now: Date;
 };
 
-const ClockScale: React.FC = () => { // text pannel
+const ClockScale: React.FC = () => { // pannel
     const scaleStyle: React.CSSProperties = {
         width: "100%",
         height: "100%"
@@ -19,14 +19,54 @@ const ClockScale: React.FC = () => { // text pannel
 };
 
 const ClockHands: React.FC<Props> = ( props ) => { // hands
+    const getTransformAttr = ( degree: number ) => {
+        const val = "rotate(" + degree + ")";
+        return val;
+    };
+
+    const [degreeSeconds, setDegreeSeconds] = useState((props.now.getSeconds() / 60) * 360 + (props.now.getMilliseconds() / 1000) * (360 / 60));
+    const [degreeMinutes, setDegreeMinutes] = useState((props.now.getMinutes() / 60) * 360 + ((degreeSeconds / 360) * (360 / 60)));
+    const [degreeHours, setDegreeHours] = useState((props.now.getHours() / 12) * 360 + ((degreeMinutes / 360) * (360 / 60)));
+
+    useEffect(() => {
+        const getTimerId = setInterval(() => {
+            setDegreeSeconds((props.now.getSeconds() / 60) * 360 + (props.now.getMilliseconds() / 1000) * (360 / 60));
+            setDegreeMinutes((props.now.getMinutes() / 60) * 360 + ((degreeSeconds / 360) * (360 / 60)));
+            setDegreeHours((props.now.getHours() / 12) * 360 + ((degreeMinutes / 360) * (360 / 60)));
+        }, 100 );
+        return () => clearInterval(getTimerId);
+    });
+
     return (
-        <h1> {props.now} </h1>
+        <svg width = "100%" height = "100%" viewBox = "-500 -500 1000 1000">
+            <defs>
+                <g id = "def_hand_long">
+                    <polygon points = "-5,-360 5,-360 10,30 -10,30" fill = "#888" />
+                </g>
+                <g id = "def_hand_short">
+                    <polygon points = "-5,-300 5,-300 10,30 -10,30" fill = "#888" />
+                </g>
+                <g id = "def_hand_second">
+                    <polygon points = "-2,-360 2,-360 3,30 -3,30" fill = "#CCC" />
+                </g>
+            </defs>
+            <use id = "hand_long" xlinkHref = "#def_hand_long" transform = {getTransformAttr(degreeMinutes)} ></use>
+            <use id = "hand_short" xlinkHref = "#def_hand_short" transform = {getTransformAttr(degreeHours)} ></use>
+            <use id = "hand_second" xlinkHref = "#def_hand_second" transform = {getTransformAttr(degreeSeconds)} ></use>
+        </svg>
     );
 };
 
 const ClockApplication: React.FC<Props> = ( props ) => { // clock app
+
+    const ClockAppStyle: React.CSSProperties = {
+        position: "relative",
+        width: "100%",
+        paddingBottom: "100%"
+    };
+
     return (
-        <section>
+        <section style = {ClockAppStyle}>
             <div> <ClockScale /></div>
             <div> <ClockHands now = {props.now} /> </div>
         </section>
@@ -35,8 +75,18 @@ const ClockApplication: React.FC<Props> = ( props ) => { // clock app
 
 
 const Clock: React.FC = () => {
+    const [now, setNow] = useState(new Date().getTime());
+    const [targetDate, setTargetDate] = useState(new Date());
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setNow(new Date().getTime());
+            setTargetDate(new Date(now));
+        }, 100);
+        return () => clearInterval(timerId);
+    })
     return (
-        <ClockApplication now = "123" />
+        <ClockApplication now = {targetDate} />
     );
 };
 
