d06404e4928a865e41e38f6291e173c30c0736ea
1an28 <14.nya.28@gmail.com>
2020-10-11 21:29:41 +0900

    connect to localStorage.

--- source/Popup/Form.tsx
+++ source/Popup/Form.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
+//import {browser} from 'webextension-polyfill-ts';
 
 type Task = Partial<{
-    beginTime: String,
-    endTime: String
+    beginTime: string,
+    endTime: string
 }>;
 
 const Form: React.FC = () => {
+    const [beginTime, setBeginTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [task, setTask] = useState<Task>();
+    let taskCount = 0;
     const handleSubmit = ( event: React.FormEvent<HTMLFormElement> ) => {
+
         const addTask: Task = {};
         addTask.beginTime = beginTime;
         addTask.endTime = endTime;
         setTask(addTask);
         event.preventDefault();
-    };
 
-    const [beginTime, setBeginTime] = useState("");
-    const [endTime, setEndTime] = useState("");
-    const [task, setTask] = useState<Task>();
+        localStorage.setItem( "task" + taskCount.toString(), addTask?.beginTime );
+    };
     
     const BeginHandleChange = ( event: React.ChangeEvent<HTMLInputElement> ) => {
         setBeginTime( event.currentTarget.value );
@@ -28,8 +32,8 @@ const Form: React.FC = () => {
     return(
         <form onSubmit = {handleSubmit}>
             <ul>
-                <li>{task?.beginTime}</li>
-                <li>{task?.endTime}</li>
+                <li>{localStorage.getItem("task0")}</li>
+                <li>{task?.toString()}</li>
             </ul>
             <input type = "time" onChange = {BeginHandleChange} />
             <input type = "time" onChange = {EndHandleChange} />
