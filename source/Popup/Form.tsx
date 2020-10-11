5f411f06b131bc8c0963543748b7592632506c79
1an28 <14.nya.28@gmail.com>
2020-10-11 20:01:14 +0900

    get the submitted time.

--- source/Popup/Form.tsx
+++ source/Popup/Form.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+type Task = Partial<{
+    beginTime: String,
+    endTime: String
+}>;
 
 const Form: React.FC = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        alert('A name was submitted');
+    const handleSubmit = ( event: React.FormEvent<HTMLFormElement> ) => {
+        const addTask: Task = {};
+        addTask.beginTime = beginTime;
+        addTask.endTime = endTime;
+        setTask(addTask);
         event.preventDefault();
     };
 
+    const [beginTime, setBeginTime] = useState("");
+    const [endTime, setEndTime] = useState("");
+    const [task, setTask] = useState<Task>();
+    
+    const BeginHandleChange = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+        setBeginTime( event.currentTarget.value );
+    };
+    const EndHandleChange = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+        setEndTime( event.currentTarget.value )
+    };
+    
     return(
         <form onSubmit = {handleSubmit}>
-            <input type = "time" />
-            <input type = "time" />
+            <ul>
+                <li>{task?.beginTime}</li>
+                <li>{task?.endTime}</li>
+            </ul>
+            <input type = "time" onChange = {BeginHandleChange} />
+            <input type = "time" onChange = {EndHandleChange} />
             <input type = "submit" />
         </form>
     );
