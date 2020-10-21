0646adaba617448100400f9247a80bf1b0d06c8c
1an28 <14.nya.28@gmail.com>
2020-10-21 20:03:50 +0900

    update a component structure.

--- source/Popup/Clock.tsx
+++ source/Popup/Clock.tsx
@@ -19,10 +19,6 @@ const ClockScale: React.FC = () => { // pannel
 };
 
 const ClockHands: React.FC<Props> = ( props ) => { // hands
-    const getTransformAttr = ( degree: number ) => {
-        const val = "rotate(" + degree + ")";
-        return val;
-    };
 
     const [degreeSeconds, setDegreeSeconds] = useState(0);
     const [degreeMinutes, setDegreeMinutes] = useState(0);
@@ -35,21 +31,10 @@ const ClockHands: React.FC<Props> = ( props ) => { // hands
     }, [props.date]);
 
     return (
-        <svg width = "100%" height = "100%" viewBox = "-500 -500 1000 1000">
-            <defs>
-                <g id = "def_hand_long">
-                    <polygon points = "-8,-440 8,-440 5,-250 -5,-250" fill = "#888" />
-                </g>
-                <g id = "def_hand_short">
-                    <polygon points = "-10,-440 10,-440 5,-300 -5,-300" fill = "#888" />
-                </g>
-                <g id = "def_hand_second">
-                    <polygon points = "-5,-440 5,-440 3,-250 -3,-250" fill = "#CCC" />
-                </g>
-            </defs>
-            <use id = "hand_long" xlinkHref = "#def_hand_long" transform = {getTransformAttr(degreeMinutes)} ></use>
-            <use id = "hand_short" xlinkHref = "#def_hand_short" transform = {getTransformAttr(degreeHours)} ></use>
-            <use id = "hand_second" xlinkHref = "#def_hand_second" transform = {getTransformAttr(degreeSeconds)} ></use>
+        <svg width = "100%" height = "100%" viewBox = "-500 -500 1000 1000" >
+            <polygon points = "-8,-440 8,-440 5,-250 -5,-250" fill = "#888" transform = { "rotate(" + degreeMinutes + ")" }/>
+            <polygon points = "-10,-440 10,-440 5,-300 -5,-300" fill = "#888" transform = { "rotate(" + degreeHours + ")" }/>
+            <polygon points = "-5,-440 5,-440 3,-250 -3,-250" fill = "#CCC" transform = { "rotate(" + degreeSeconds + ")" }/>
         </svg>
     );
 };
@@ -65,8 +50,6 @@ const DigitalClock: React.FC<Props> = ( props ) => {
 const SchedObject: React.FC = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
 
-    
-
     useEffect(() => {
         const item = localStorage.getItem("tasks");
         if (item) {
@@ -81,38 +64,25 @@ const SchedObject: React.FC = () => {
             const tasksItem: Task[] = JSON.parse(item);
             setTasks(tasksItem);
         };
-    }, [localStorage.getItem("tasks")]);
-
-    
-    const getTransformAttr = ( degree: number ) => {
-        const val = "rotate(" + degree + ")";
-        return val;
-    };
-    
+    }, [localStorage.getItem("tasks")]);    
 
     const beginTimeToDegree = (beginTime: string) => {
         const result = beginTime.split(":");
         const hour = parseInt(result[0]);
         const minute = parseInt(result[1]);
 
-        return (hour / 12) * 360 + ((minute / 360) * (360 / 60));
+        return ( hour * (360 / 12)) + ( minute * ((360 / 12) / 60) );
     };
 
     return (
         <svg width = "100%" height = "100%" viewBox = "-500 -500 1000 1000">
-            <defs>
-                <g id = "def_hand_task">
-                    <polygon points = "-10,-440 10,-440 5,-300 -5,-300" fill = "black" />
-                </g>
-            </defs>
             {
                 tasks.map((task, index) => {
                     return(
-                        <use id = { "hand_task" + index } xlinkHref = "#def_hand_task" transform = {getTransformAttr(beginTimeToDegree(task.beginTime))} ></use>
+                        <polygon id = { "tasks" + index } points = "-10,-440 10,-440 5,-300 -5,-300" fill = "black" transform = {"rotate(" + beginTimeToDegree(task.beginTime) + ")"} />
                     );
                 })
             }
-            
         </svg>
     );
 };
