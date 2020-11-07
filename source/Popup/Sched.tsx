85421fbafb292b5cbcecf96cc21d8736b8943f2a
1an28 <14.nya.28@gmail.com>
2020-11-07 21:40:21 +0900

    Added a list along the material design..

--- source/Popup/Sched.tsx
+++ source/Popup/Sched.tsx
@@ -1,19 +1,33 @@
 import React from "react";
-import List, {ListItem, ListItemText} from '@material/react-list';
+import List, {ListItem, ListItemText, ListItemGraphic} from '@material/react-list';
+import MaterialIcon from '@material/react-material-icon';
+import IconButton from '@material/react-icon-button';
 
 import '@material/react-list/dist/list.css';
+import '@material/react-icon-button/dist/icon-button.css';
+
 
 const Sched: React.FC = () => {
+
+    const listItemCss: React.CSSProperties = {
+        display: "flex",
+        justifyContent: "space-between"
+
+    }
+
     return (
         <List>
-            <ListItem>
-            <   ListItemText primaryText='Photos'/>
+            <ListItem style = {listItemCss}>
+                <ListItemText primaryText='A'/>
+                <IconButton>
+                    <ListItemGraphic graphic={<MaterialIcon icon='remove_circle_outline'/>}/>
+                </IconButton>
             </ListItem>
             <ListItem>
-                <ListItemText primaryText='Recipes'/>
+                <ListItemText primaryText='B'/>
             </ListItem>
             <ListItem>
-                <ListItemText primaryText='Work'/>
+                <ListItemText primaryText='C'/>
             </ListItem>
         </List>
     );
