34898c338a91eaabf0ce1f5c4edb097cca86df93
1an28 <14.nya.28@gmail.com>
2020-12-13 10:56:40 +0900

    syncfusionのスケジューラを追加

--- source/Popup/Calendar.tsx
+++ source/Popup/Calendar.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
+import {ScheduleComponent, Day, Week, WorkWeek, Month, Inject} from '@syncfusion/ej2-react-schedule';
+
 const Calendar: React.FC = () => {
     return (
-        <div>Calendar</div>
+        <div className='control-wrapper'>
+            <ScheduleComponent height='350px' selectedDate={new Date()}>
+                <Inject services={[Day, Week, WorkWeek, Month]} />
+            </ScheduleComponent>
+        </div>
     );
 };
 
